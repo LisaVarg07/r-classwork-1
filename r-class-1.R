667a45e12990006a042b6d67edca60ecbb220396
LisaVarg07 <79004880+LisaVarg07@users.noreply.github.com>
2021-02-25 16:12:24 +0530

    Create r-class-1.R

---
+++ r-class-1.R
@@ -0,0 +1,35 @@
+print("Hello, World")
+2+3
+2+6
+a = 8
+b <- 19
+a=b
+a==b
+(a=444*44)
+(x<-2+3)
+nummy<-c(2,3,4)
+nummy_int <- c(1L,2L,3L)
+typeof(nummy)
+typeof(nummy_int)
+is.numeric(nummy)
+is.numeric(nummy_int)
+is.double(nummy)
+logicals <- c(TRUE,F,TRUE,T, FALSE)
+money_in_chars <- c("20","35","33")
+typeof(money_in_chars)
+money_money <- as.numeric(money_in_chars)
+money_money
+typeof(money_money)
+money_logical <- as.logical(money_money)
+money_logical
+c(2,4,"hello",TRUE)
+c(2,4,TRUE)
+month.abb
+month.abb[2]
+month.abb[4:7]
+month.abb[c(7,8,12)]
+months<-c("Jan", "Feb", "March")
+months
+days<-c("monday", "Tuesday")
+more_months<-c(months,"April", "May",days)
+more_months
